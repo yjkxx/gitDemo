b64956fc377983c27a590fca35bfe1a26602a30a
Young <yjkxcvii@gmail.com>
2023-02-15 01:23:23 +0900

    added a comment to VC

---
+++ dummyApp/dummyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dummyApp
+//
+//  Created by YoungJoon Kim on 2/15/23.
+//
+
+import UIKit
+// MARK
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
